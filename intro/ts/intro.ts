3b97343032b61337d3dcafdc03c4d80fbffcd06d
CesarQuint <pedrocesarquintero001@gmail.com>
2023-11-21 13:17:49 -0600

    Patrones con Ts

--- intro/ts/intro.ts
+++ intro/ts/intro.ts
@@ -1,1 +1,31 @@
-console.log("Hola")
+console.log('Hola');
+
+class Drink {
+  private name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  info(): string {
+    return this.name;
+  }
+}
+
+const drink = new Drink('agua');
+console.log(drink.info());
+
+//*Herencia
+
+class Beer extends Drink {
+  private alcohol: number;
+
+  constructor(name: string, alcohol: number) {
+    super(name);
+    this.alcohol = alcohol;
+  }
+  info(): string {
+    return super.info() + ' ' + this.alcohol;
+  }
+}
+
+//!Interfaces
